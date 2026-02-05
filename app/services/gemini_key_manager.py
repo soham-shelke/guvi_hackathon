2ad8502aa11a5ccb8d1824a571b6c52664bc50e9
soham-shelke <soham3105@gmail.com>
2026-02-05 23:20:05 +0530

    added api rotation

--- app/services/gemini_key_manager.py
+++ app/services/gemini_key_manager.py
@@ -3,20 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-keys_string = os.getenv("GEMINI_API_KEYS", "")
-GEMINI_KEYS = [k.strip() for k in keys_string.split(",") if k.strip()]
+GEMINI_KEYS = [
+    os.getenv("GEMINI_API_KEY_1"),
+    os.getenv("GEMINI_API_KEY_2"),
+]
 
-current_key_index = 0
+key_index = 0
 
 
 def get_next_key():
-    global current_key_index
+    global key_index
 
-    if not GEMINI_KEYS:
-        raise Exception("No Gemini keys found")
+    key = GEMINI_KEYS[key_index]
 
-    key = GEMINI_KEYS[current_key_index]
-
-    current_key_index = (current_key_index + 1) % len(GEMINI_KEYS)
+    key_index = (key_index + 1) % len(GEMINI_KEYS)
 
     return key
